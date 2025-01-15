907e95a1c4769eb36b9c95c70570583edb6dce7d
Uriel Abraham <abraham2160@hotmail.com>
2025-01-15 13:52:40 -0700

    Actividad 1
    Ejemplos

---
+++ Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
@@ -0,0 +1,15 @@
+package com.mycompany.actividad1;
+public class Actividad1 {
+
+    public static void main(String[] args) {
+        String texto;
+        texto = "prueba";
+        System.out.println(texto);
+        int numero1, numero2, num;
+        numero1 = 1;
+        numero2 = 3;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
