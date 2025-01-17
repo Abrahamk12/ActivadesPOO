5ff5b280b49baf6dd6b9aee0a11721a34b58a8c6
Uriel Abraham <abraham2160@hotmail.com>
2025-01-17 08:06:16 -0700

    Arreglos
    Ejemplo de uso de arreglos, acomodando números de mayor a menor

--- Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
+++ Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
@@ -2,14 +2,26 @@ package com.mycompany.actividad1;
 public class Actividad1 {
 
     public static void main(String[] args) {
-        String texto;
-        texto = "prueba";
-        System.out.println(texto);
-        int numero1, numero2, num;
-        numero1 = 1;
-        numero2 = 3;
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+        /*
+            Se quiere acomodar el arreglo de mayor
+            a menor
+        */
+        int arreglo [] = {4, 15, 1, 10, 30, 8, 2};
+        int arreglo2 [] = new int[arreglo.length];
+        int mayor = 0, indice = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Vueltas en i:"+i+"\tel valor de indice:"+indice );
+            for (int j = 0; j < arreglo.length; j++) {
+                if(mayor < arreglo[j]){
+                    mayor = arreglo[j];
+                    indice = j;
+                    System.out.println("El valor del indice dentro del if: "+indice);
+                }
+            }
+            arreglo2[i] = mayor;
+            mayor = 0;
+            arreglo[indice] = 0;
+            System.out.println("Vueltas en i:"+i+"\tel valor de indice:"+indice );
         }
     }
 }
