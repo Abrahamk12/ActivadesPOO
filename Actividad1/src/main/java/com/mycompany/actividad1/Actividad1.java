331f22836c78d68775be13aabda9b556cb6f7478
Uriel Abraham <abraham2160@hotmail.com>
2025-01-31 08:52:05 -0700

    Ejemplo ventana

--- Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
+++ Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
@@ -1,15 +1,13 @@
 package com.mycompany.actividad1;
-
 import javax.swing.JOptionPane;
 
 public class Actividad1 {
 
     public static void main(String[] args) {
-     // JOptionPane.showMessageDialog(null, "Hola mundo");
-        String name = JOptionPane.showInputDialog("What's your name?");
-        System.out.println(name);
-        //Castear informacion
-        double n = Double.parseDouble(name); 
-        int nn = Integer.parseInt(name);
+        //JOptionPane.showMessageDialog(null, "Hola mundo");
+        //String a = JOptionPane.showInputDialog("Ingrese una palabra");
+        //System.out.println(a);
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
     }
 }
